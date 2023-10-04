a15e3ef434173d39ebc9abdd35f07a01bdca6bde
Koen van Dijk <kjw.vandijk@gmail.com>
2023-10-04 14:00:15 +0200

    stream test 2

--- test/stream_bug_test.exs
+++ test/stream_bug_test.exs
@@ -5,6 +5,7 @@ defmodule StreamBugTest do
   @image_path "#{File.cwd!()}/church.jpg"
   @copy_path "#{File.cwd!()}/copy.jpg"
   @stream_path "#{File.cwd!()}/stream.jpg"
+  @stream_path2 "#{File.cwd!()}/stream2.jpg"
   @lorem_copy_path "#{File.cwd!()}/lorem_copy.txt"
   @hex "#{File.cwd!()}/hex.txt"
   @hex_copy "#{File.cwd!()}/hex_copy.txt"
@@ -12,6 +13,12 @@ defmodule StreamBugTest do
   test "stream the image" do
     File.stream!(@image_path)
     |> Enum.into(File.stream!(@stream_path))
+  end
+
+  test "stream the image 2" do
+    File.stream!(@image_path)
+    |> Stream.into(File.stream!(@stream_path2))
+    |> Stream.run()
   end
 
   test "hex" do
